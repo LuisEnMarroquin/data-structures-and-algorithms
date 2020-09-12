5fcf1842c4b22380427138d917a10baa39518637
LuisEnMarroquin <mluis651@gmail.com>
2020-09-11 21:17:35 -0500

    Restart iteration if available index is smaller than module

--- hashTable.js
+++ hashTable.js
@@ -10,19 +10,23 @@ let unsorted = [
   'Rae',
   'Max',
   'Tod',
-  // 'Elm'
+  'Elm'
 ]
 
 let sorted = []
 
 unsorted.forEach(() => sorted.push(''))
 
+// function hashModule (term) {
+
+// }
+
 unsorted.forEach(item => {
   let chars = item.split('')
 
   let sumCode = 0
   chars.forEach(char => {
-    sumCode += char.charCodeAt(0);
+    sumCode += char.charCodeAt(0)
   })
 
   let moduleCode = sumCode % unsorted.length
@@ -30,24 +34,25 @@ unsorted.forEach(item => {
   if (sorted[moduleCode] === '') {
     sorted[moduleCode] = item
   } else {
+    let excedent = 0
     let condition = true
 
     while (condition) {
       moduleCode++
-
-      if (sorted[moduleCode] === '') {
-
-        // if (moduleCode >= unsorted.length) {
-        //   condition = false
-        // } else {
+      if (moduleCode <= unsorted.length - 1) {
+        if (sorted[moduleCode] === '') {
           sorted[moduleCode] = item
           condition = false
-        // }
-
+        }
+      } else {
+        excedent++
+        if (sorted[excedent] === '') {
+          sorted[excedent] = item
+          condition = false
+        }
       }
     }
   }
-
 })
 
 console.log(sorted)
